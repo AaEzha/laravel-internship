ab53ab9326fa664a6e9c4718d6321b536f36520e
aaezha <aaezha@gmail.com>
2020-12-03 03:49:23 +0700

    Prepare menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,12 +58,11 @@
         </div>
 
         @can('admin')
-            <li class="nav-item {{ Nav::isRoute('member.index') }}">
-                <a class="nav-link" href="{{ route('member.index') }}">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Users</span>
-                </a>
-            </li>
+            @include('menu.admin')
+        @endcan
+
+        @can('member')
+            @include('menu.member')
         @endcan
 
         <!-- Nav Item - Profile -->
